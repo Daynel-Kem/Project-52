e397e777d0c4ebdcbdf399e5ab485ee025f7bc87
Daynel-Kem <dani3lkimm2005@gmail.com>
2025-05-02 15:18:42 -0400

    fixed text cycle

--- W2-Portfolio-Updated/portfolio/src/app/components/Header.jsx
+++ W2-Portfolio-Updated/portfolio/src/app/components/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { assets } from '../../../assets/assets'
 import TextLoop from 'react-text-loop'
 import {motion} from 'motion/react'
+import { RotateWords } from './Word-Loop'
 
 const Header = () => {
   return (
@@ -17,15 +18,16 @@ const Header = () => {
             <Image src={assets.hand_icon} alt="" className="rounded-full w-6"/>
           </motion.div>
         </h3>
-        <div className="flex flex-col justify-end sm:width-[80%]">
+        <div className="flex flex-col sm:width-[80%]">
           <h1 className='text-5xl sm:text-6xl lg:text-[66px] Ovo text-center sm:text-left'>
               I'm a           
           </h1>
-          <TextLoop springConfig={{stiffness: 180, damping: 20}} noWrap={false} className='text-6xl text-left'>
+          <RotateWords text="" words={["Student", "Developer", "Friend"]} className='' />
+          {/* <TextLoop springConfig={{stiffness: 180, damping: 20}} noWrap={false} className='text-6xl text-left'>
               <h1 className='sm:text-6xl lg:text-[90px] Ovo text-amber-400'> Student</h1>
               <h1 className='sm:text-6xl lg:text-[90px] Ovo text-green-300'> Developer</h1>
               <h1 className='sm:text-6xl lg:text-[90px] Ovo text-red-800'> Friend</h1>
-          </TextLoop>
+          </TextLoop> */}
         </div>
         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
           <a href="#contact" className='text-center px-10 py-2 border border-white rounded-full bg-black text-white flex items-center gap-2'>Contact Me <Image src={assets.right_arrow_white} alt="" className="w-4"/></a>
---
+++ W2-Portfolio-Updated/portfolio/src/app/components/Word-Loop.jsx
@@ -0,0 +1,38 @@
+"use client"
+import * as React from "react"
+import { AnimatePresence, motion } from "framer-motion"
+ 
+export function RotateWords({
+  text = "Rotate",
+  words = ["Word 1", "Word 2", "Word 3"],
+}) {
+  const [index, setIndex] = React.useState(0)
+ 
+React.useEffect(() => {
+const interval = setInterval(() => {
+setIndex((prevIndex) => (prevIndex + 1) % words.length)
+}, 5000)
+// Clean up interval on unmount
+return () => clearInterval(interval)
+}, [])
+return (
+ 
+<div className="text-7xl text-left Ovo">
+  {text}{' '}
+  <AnimatePresence mode="wait">
+    <motion.h3
+      key={words[index]}
+      initial={{ opacity: 0, y: -40 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 40 }}
+      transition={{ duration: 0.5 }}
+      className={`Ovo sm:text-6xl lg:text-[90px] 
+                ${index==0 ? "text-amber-400" : ""} 
+                ${index==1 ? " text-green-300" : ""}
+                ${index==2 ? "text-red-800" : ""}`}
+    >
+      {words[index]}
+    </motion.h3>
+  </AnimatePresence>
+</div>
+) }
